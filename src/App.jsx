4481c01c21469c9195d7b0cdc7128f4f493a91a8
anko-developer <mikkeller@naver.com>
2024-06-26 10:58:04 +0900

    useTitle hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Outlet } from "react-router-dom";
+import { FallingLines } from "react-loader-spinner";
 import useTabs from "./hooks/useTabs";
+import useTitle from "./hooks/useTitle";
 
 const content = [
   {
@@ -15,16 +17,31 @@ const content = [
 
 export default function App() {
   const { changeItem, currentItem } = useTabs(0, content);
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => {
+    titleUpdater("Home!");
+  }, 5000);
   return (
     <>
-      <Outlet />
-      <div>Custom Hook</div>
-      {content.map((section, index) => (
-        <button onClick={() => changeItem(index)} key={section.tab}>
-          {section.tab}
-        </button>
-      ))}
-      <div>{currentItem.content}</div>
+      <Suspense
+        fallback={
+          <FallingLines
+            color="#4fa94d"
+            width="100"
+            visible={true}
+            ariaLabel="falling-circles-loading"
+          />
+        }
+      >
+        <Outlet />
+        <div>Custom Hook</div>
+        {content.map((section, index) => (
+          <button onClick={() => changeItem(index)} key={section.tab}>
+            {section.tab}
+          </button>
+        ))}
+        <div>{currentItem.content}</div>
+      </Suspense>
     </>
   );
 }
---
+++ src/hooks/useTitle.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
+export default useTitle;
+
+// const titleUpdater = useTitle("Loading...");
+// setTimeout(() => {
+//   titleUpdater("Home!");
+// }, 5000);
