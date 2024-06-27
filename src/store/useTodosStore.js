db70828ed6c283ddcccf93b2f9bf5ba4d230104b
anko-developer <mikkeller@naver.com>
2024-06-27 15:42:42 +0900

    api 경로 수정

--- src/store/useTodosStore.js
+++ src/store/useTodosStore.js
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
-import MemoApi from "../api/memoApi";
+import MemoApi from "../service/memoApi";
 
 const memoApi = new MemoApi();
 
